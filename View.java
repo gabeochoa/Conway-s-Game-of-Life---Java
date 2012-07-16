089162dc49f42939276447467fdfe17e656961b9
Gabe Ochoa <gabe1118@yahoo.com>
2012-07-16 18:28:50 -0400

    Fixed Code Errors / Got rid of unused code

--- View.java
+++ View.java
@@ -1,14 +1,9 @@
-import java.awt.Container;
-import java.awt.Cursor;
 import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Random;
-import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
@@ -16,13 +11,14 @@ import javax.swing.JFrame;
 
 public class View extends JFrame
 {
+	private static final long serialVersionUID = 1L;
+	
 	int x;
 	int y;
 	int[][] board;
 	Point playerLoc;
 	BufferedImage bg;
 	BufferedImage enemy;
-	//BufferedImage player;
 	
 	
 	public View(int x, int y) throws IOException
@@ -32,15 +28,10 @@ public class View extends JFrame
 		this.y = y;
 		this.setBounds(100, 90, x, y);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Container pane = this.getContentPane();
-		pane.setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
-		//playerLoc = new Point();
 		board = new int[x/25][y/25];
 		fillAra();
-		//this.addKeyListener(new KBListener(playerLoc, board));
 		bg = ImageIO.read(new File("background.png"));
 		enemy = ImageIO.read(new File("enemy.png"));
-		//player = ImageIO.read(new File("player.png"));
 		
 		
 		this.setVisible(true);
@@ -118,40 +109,19 @@ public class View extends JFrame
 		}
 	}
 	
-	private void fillBoard() throws FileNotFoundException 
-	{
-		Scanner reader = new Scanner(new File("board.txt"));
-		
-		for(int i = 0; i < x/25; i++)
-		{
-			for(int j = 0; j < x/25 ; j++)
-			{
-				board[i][j] = reader.nextInt();
-				if(board[i][j] == 1)
-				{
-					playerLoc.x = j;
-					playerLoc.y = i;
-				}
-				//System.out.println(board[i][j]);
-			}
-		}
-		reader.close();
-	}
-
+	
 	public void draw(Graphics g) 
 	{
 		int x1 = 0;
 		int y1 = 0;
-		//System.out.println(x/25);
+		
 		for(int i = 0; i < (x/25); i++, y1 = y1 + 25)
 		{
 			for(int j = 0; j < (x/25); j++, x1 = x1 + 25 )
 			{
-				//System.out.println("x1:" + x1 + ",y1: " + y1);
 				switch(board[i][j])
 				{
 					case 0:  g.drawImage(bg, x1, y1, null); break;
-					case 1:  g.drawImage(player, x1, y1, null); break;
 					case 2:  g.drawImage(enemy, x1, y1, null);
 				}
 			}
